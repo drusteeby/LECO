5f599a6aae618579076eaf3ae0e77b5ddeec09d4
drusteeby <drusteeby@gmail.com>
2023-08-04 16:32:19 -0400

    Fleshing out UI

---
+++ CanvasItemTemplateSelector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace LECO
+{
+    public class CanvasItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate CityTemplate { get; set; }
+
+        public DataTemplate LineTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is LineViewModel)
+            {
+                return LineTemplate;
+            }
+            if (item is CityViewModel)
+            {
+                return CityTemplate;
+            }
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
