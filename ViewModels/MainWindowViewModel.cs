8fae21d0f5f4d00711b42e1ff64753efcdfa6509
drusteeby <drusteeby@gmail.com>
2023-08-04 22:46:35 -0400

    adding cities to map

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -5,8 +5,10 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
+using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,7 +20,23 @@ namespace LECO.ViewModels
         {
             _fileName = String.Empty;
             LoadedCities = new ObservableCollection<City>();
+            SelectedCities = new ObservableCollection<City>();
+            MapItems = new ObservableCollection<CanvasItemViewModelBase>();
             SelectFile = new DelegateCommand(OnSelectFileCommand);
+
+            SelectedCities.CollectionChanged += SelectedCities_CollectionChanged;
+        }
+
+        public ObservableCollection<CanvasItemViewModelBase> MapItems { get; set; }
+
+        private void SelectedCities_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            MapItems.Clear();
+            foreach(var city in SelectedCities) 
+            {   
+                MapItems.Add(new CityViewModel(city));
+            }
+
         }
 
         private void OnSelectFileCommand()
@@ -68,10 +86,46 @@ namespace LECO.ViewModels
         public ObservableCollection<City> SelectedCities { get; set; }
 
         private City _selectedCity;
-        public City SelectedCity 
+        public City SelectedCity
         {
             get => _selectedCity;
             set => SetProperty(ref _selectedCity, value);
         }
+
+        private bool SelectedCityNotNull() => SelectedCity is not null;
+
+        private DelegateCommand _addSelectedToDestinationCommand;
+        public DelegateCommand AddSelectedToDestinationCommand => _addSelectedToDestinationCommand ??= new DelegateCommand(ExecuteAddSelectedToDestinationCommand, canExecuteMethod: SelectedCityNotNull).ObservesProperty(() => SelectedCity);
+
+        void ExecuteAddSelectedToDestinationCommand()
+        {
+            if (SelectedCities.Contains(SelectedCity))
+            {
+                return;
+            }
+
+            SelectedCities.Add(SelectedCity);
+        }
+
+        private City _homeCity;
+        public City HomeCity
+        {
+            get => _homeCity;
+            set => SetProperty(ref _homeCity, value);
+        }
+
+        private DelegateCommand _setHomeCityCommand;
+        public DelegateCommand SetHomeCityCommand => _setHomeCityCommand ??= new DelegateCommand(ExecuteSetHomeCityCommand, canExecuteMethod: SelectedCityNotNull).ObservesProperty(() => SelectedCity);
+
+        void ExecuteSetHomeCityCommand() => HomeCity = SelectedCity;
+
+        private DelegateCommand _startCalculationCommand;
+        public DelegateCommand StartCalculationCommand => (_startCalculationCommand ??= new DelegateCommand(ExecuteStartCalculationCommand, canExecuteMethod: () => SelectedCities.Count > 2)).ObservesProperty(() => SelectedCities);
+
+        void ExecuteStartCalculationCommand()
+        {
+
+        }
+
     }
 }
